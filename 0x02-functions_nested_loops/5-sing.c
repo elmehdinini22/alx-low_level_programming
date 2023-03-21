28d926d80069efccbba4af4de9d133b462b41fb7
elmehdinini22 <elmehdinini1@gmail.com>
2023-03-21 15:27:56 -0700

    messag

---
+++ 0x02-functions_nested_loops/5-sing.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_sign - print the number's sign
+* @n: input as integer
+* Return: 1, 0, or -1
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+return (0);
+}
